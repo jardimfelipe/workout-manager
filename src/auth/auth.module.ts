aec8988cb9956aa877d9e34a7135ac050328bfc2
FELIPE JARDIM <felipejardim@MacBook-Air-de-FELIPE.local>
2023-09-12 19:20:41 -0300

    chore: test expire token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -21,7 +21,7 @@ import { JwtStrategy } from "./jwt.strategy";
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get("JWT_SECRET"),
-        signOptions: { expiresIn: 3600 },
+        signOptions: { expiresIn: 10 },
       }),
     }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
